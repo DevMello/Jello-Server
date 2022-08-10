46b17703236d33d4889e6ac95297b86df0c64a9f
DevMello <69360966+DevMello@users.noreply.github.com>
2022-08-09 19:51:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,7 @@ const yas = require('youtube-audio-server')
 yas.setKey('AIzaSyBNpjmwdyPybDRJS0YceMc2tcuxgXoF_Bc')
 const port = 7331
 
-yas.listen(port, () => {
+yas.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port http://localhost:${port}.`)
 })
 
-const express = require("express");
-const app = express();
-
-app.get("/express/express", (request, response) => {
-    response.send("Hi there");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Listen on the port 3000...");
-});
